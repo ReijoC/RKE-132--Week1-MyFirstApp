a30b152ee3712cca0cb2734af69e103851ca83c9
Reijo P <rpajussa@tktk.ee>
2023-03-27 21:43:38 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// comment
+
+// r kysib nime
+// r tervitab
+
+Console.WriteLine("enter y name");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+Console.WriteLine($"Hello {userName} !");
+
+
